020231063a77be247c4abee9ffe7c4a242ad4a17
Jazzy Guo <guojazzy@gmail.com>
2023-07-28 20:03:21 -0400

    feat: add some description copy to index page for unauthed user

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useAuth } from "@/contexts/AuthContext";
-import { HOME_URL } from "@/config";
+import { HOME_URL, MAX_QUESTIONS, MAX_ANSWERS } from "@/config";
 
 const IndexPage = () => {
   const router = useRouter();
@@ -14,8 +14,10 @@ const IndexPage = () => {
   }
 
   return (
-    <div className="text-center">
-      Log in to get started!
+    <div className="text-center max-w-sm mt-8 m-auto">
+      <p>Quiz builder allows you to create and save quizzes.</p>
+      <p>You can have up to {MAX_QUESTIONS} questions with {MAX_ANSWERS} answers each, with the ability to mark one or multiple answers as correct.</p>
+      <p>The quiz can be saved as a draft to be edited later, or published so that you can generate a unique share url to let others take your quiz.</p>
     </div>
   )
 }
